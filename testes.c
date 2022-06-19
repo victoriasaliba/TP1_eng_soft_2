d1d7bc6a1e85313468279215bd3f435e8e14354c
Leandro Souza <LMarqu23@its.jnj.com>
2022-06-18 22:06:45 -0300

    Add unit 11 unit tests
    They cover all possible outputs of functions located in common.c

--- testes.c
+++ testes.c
@@ -2,59 +2,101 @@
 #include "inc_server.h"
 #include "sput-1.4.0/sput.h"
 
-/*
- * count_vowels() counts the vowels present in a given string.
- *
- * While the function basically works as expected, it recognizes
- * [aeiou] as vowels only and erroneously does not take uppercase
- * vowels into account.
- */
-static int count_vowels(const char *s)
-{
-  const char *cp = s;
-  int count = 0;
+int setup(char *func, char *argv, char* argc) {
+  struct sockaddr_storage storage;
 
-  while (*cp)
-  {
-    if (*cp == 'a' || *cp == 'e' || *cp == 'i' ||
-        *cp == 'o' || *cp == 'u')
-    {
-      count++;
-    }
-
-    cp++;
+  if(0 == strcmp("defineProtocolo", func)) {
+    return defineProtocolo(argv, argc, &storage);
   }
-
-  return count;
+  else if(0 == strcmp("defineProtocoloServidor", func)) {
+    return defineProtocoloServidor(argv, argc, &storage);
+  }
+  else {
+    return EXIT_FAILURE;
+  }
 }
 
-static void test_vowels_present()
+/**
+ * Client tests
+ */
+static void test_client_address_ipv4()
 {
-  sput_fail_unless(count_vowels("book") == 2, "book == 2v");
-  sput_fail_unless(count_vowels("hand") == 1, "hand == 1v");
-  sput_fail_unless(count_vowels("test") == 1, "test == 1v");
-  sput_fail_unless(count_vowels("Peter") == 2, "Peter == 2v");
-  sput_fail_unless(count_vowels("Apu") == 2, "Apu == 2v");
+  int test = setup("defineProtocolo", "127.0.0.1", "5151");
+  sput_fail_unless(test == 0, "Endereço IPv4 válido");
 }
 
-static void test_no_vowels_present()
+static void test_client_address_ipv4_invalido()
 {
-  sput_fail_unless(count_vowels("GCC") == 0, "GCC == 0v");
-  sput_fail_unless(count_vowels("BBC") == 0, "BBC == 0v");
-  sput_fail_unless(count_vowels("CNN") == 0, "CNN == 0v");
-  sput_fail_unless(count_vowels("GPS") == 0, "GPS == 0v");
-  sput_fail_unless(count_vowels("Ltd") == 0, "Ltd == 0v");
+  int test = setup("defineProtocolo", "127.0.0.1.152", "5151");
+  sput_fail_unless(test == -1, "Endereço IPv4 inválido : Mais de 3 pontos");
+  test = setup("defineProtocolo", "127.256.0.1", "5151");
+  sput_fail_unless(test == -1, "Endereço IPv4 inválido : Possui valor acima de 255");
+}
+
+static void test_client_address_ipv6()
+{
+  int test = setup("defineProtocolo", "59FB::1005:CC57:6571", "5151");
+  sput_fail_unless(test == 0, "Endereço IPv6 válido");
+}
+
+static void test_client_address_ipv6_invalido()
+{
+  int test = setup("defineProtocolo", "56FE::2159:5BBC::6594", "5151");
+  sput_fail_unless(test == -1, "Endereço IPv6 inválido");
+}
+
+static void test_client_porta_invalida()
+{
+  int test = setup("defineProtocolo", "127.0.0.1", "0");
+  sput_fail_unless(test == -1, "Porta invalida com Endereço IPv4 válido");
+  test = setup("defineProtocolo", "::1", "0");
+  sput_fail_unless(test == -1, "Porta invalida com Endereço IPv6 válido");
+}
+
+/**
+ * Server tests
+ */
+static void test_server_protocolo_ipv4()
+{
+  int test = setup("defineProtocoloServidor", "v4", "5151");
+  sput_fail_unless(test == 0, "Protocolo IPv4");
+}
+
+static void test_server_protocolo_ipv6()
+{
+  int test = setup("defineProtocoloServidor", "v6", "5151");
+  sput_fail_unless(test == 0, "Protocolo IPv6");
+}
+
+static void test_server_protocolo_invalido()
+{
+  int test = setup("defineProtocoloServidor", "v5", "5151");
+  sput_fail_unless(test == -1, "Protocolo Invalido : Sistema só suporta IPv4 e IPv6");
+}
+
+static void test_server_porta_invalida()
+{
+  int test1 = setup("defineProtocoloServidor", "v4", "0") == -1;
+  int test2 = setup("defineProtocoloServidor", "v6", "0") == -1;
+  sput_fail_unless((test1 && test2), "Porta invalida com protocolo válido");
 }
 
 int main(int argc, char *argv[])
 {
   sput_start_testing();
 
-  sput_enter_suite("count_vowels(): Vowels Present");
-  sput_run_test(test_vowels_present);
+  sput_enter_suite("Client -> defineProtocolo()");
+  sput_run_test(test_client_address_ipv4);
+  sput_run_test(test_client_address_ipv4_invalido);
+  sput_run_test(test_client_address_ipv6);
+  sput_run_test(test_client_address_ipv6_invalido);
+  sput_run_test(test_client_porta_invalida);
 
-  sput_enter_suite("count_vowels(): No Vowels Present");
-  sput_run_test(test_no_vowels_present);
+  sput_enter_suite("Server -> defineProtocoloServidor() :");
+  sput_run_test(test_server_protocolo_ipv4);
+  sput_run_test(test_server_protocolo_ipv6);
+  sput_run_test(test_server_protocolo_invalido);
+  sput_run_test(test_server_porta_invalida);
 
   sput_finish_testing();
 
